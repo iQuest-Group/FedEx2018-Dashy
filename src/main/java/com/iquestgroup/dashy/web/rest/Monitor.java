785a89d1afddacf6aab524b8ec3b4375a5d8f3a3
Traian Lupan <Traian.Lupan@iquestgroup.com>
2018-11-16 11:41:33 +0200

    Adding the monitoring controller

---
+++ src/main/java/com/iquestgroup/dashy/web/rest/Monitor.java
@@ -0,0 +1,37 @@
+package com.iquestgroup.dashy.web.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Returns the status of the machines in the environment
+ */
+@RestController
+public class Monitor {
+
+
+    @GetMapping(path = "/monitor")
+    public String getEnvironmentStatus(){
+        return "{\n"
+            + "  \"id\": \"15\",\n"
+            + "  \"machines\": [\n"
+            + "    {\n"
+            + "      \"service_name\": \"gendg\",\n"
+            + "      \"version\": \"0.5.0-SNAPSHOT\",\n"
+            + "      \"type\": \"spring_boot | other_app\",\n"
+            + "      \"status\": \"up | down\",\n"
+            + "      \"details_url\": \" http://glances | http://spring_boot_admin_app_details\"\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"service_name\": \"rest_jms_bridge\",\n"
+            + "      \"version\": \"1.5.0-SNAPSHOT\",\n"
+            + "      \"type\": \"spring_boot | other_app\",\n"
+            + "      \"status\": \"up\",\n"
+            + "      \"details_url\": \" http://glances | http://spring_boot_admin_app_details\"\n"
+            + "    }\n"
+            + "  ],\n"
+            + "  \"schemaVersion\": \"1.0\"\n"
+            + "}";
+    }
+
+}
